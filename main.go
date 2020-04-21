f1ae3f40b4322a6e974d04aded2668bd58662150
945188363 <niujingheng1997@163.com>
2020-04-21 17:54:27 +0800

    包引入

--- main.go
+++ main.go
@@ -5,16 +5,14 @@ import (
 	"GoMicro-Project/Service/Micros"
 	"GoMicro-Project/ServiceImpl"
 	"github.com/micro/go-micro"
-	"github.com/micro/go-micro/registry"
-	"github.com/micro/go-micro/registry/mdns"
 	"github.com/micro/go-micro/web"
 )
 
 func main() {
 	// 注册到mdns
-	mdnsReg := mdns.NewRegistry(
-		registry.Addrs("192.168.2.1"),
-	)
+	// mdnsReg := mdns.NewRegistry(
+	// 	registry.Addrs("192.168.2.1"),
+	// )
 	// 注册到consul
 	// consulReg := consul.NewRegistry(
 	// 	registry.Addrs("192.168.2.1"),
@@ -30,7 +28,7 @@ func main() {
 		micro.Name("ProdServiceRPC"),
 		micro.Address(":8881"),
 		// micro.Registry(consulReg),
-		micro.Registry(mdnsReg),
+		// micro.Registry(mdnsReg),
 	)
 	// 注册并启动rpc服务
 	rpcService.Init()
